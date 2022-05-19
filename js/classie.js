89d6abc7addfda0b7fa33da6c8bf5f9ec83bf87e
Pranav <sonawanepranav19@gmail.com>
2022-05-20 00:22:25 +0530

    classie file changed

--- js/classie.js
+++ js/classie.js
@@ -5,8 +5,8 @@
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
+ * classie.toggle( elem, 'my-class' )*/
+ 
 
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
